82a7213ce2ef008b37a1084d769f146e49f3b544
vamsibudatidabot <vamsi@dabot.ai>
2024-01-16 13:12:47 +0530

    upgrades version

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import ConfigConstructor from 'config';
+import ConfigConstructor from './config';
 import utils from './utils';
 
 const Utils = { CommonUtilities: utils.CommonUtilities, StandardizeQueryParams: utils.StandardizeQueryParams }
 
-export type { Config } from 'config';
+export type { Config } from './config';
 
 export { Utils, ConfigConstructor };
